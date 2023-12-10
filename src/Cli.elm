bd1af436bb8d4b06b435474d774e89e3362f1aa8
Jon Thompson <jon.thompson@vendr.com>
2023-12-10 14:00:50 -0500

    add readFile

--- src/Cli.elm
+++ src/Cli.elm
@@ -1,5 +1,8 @@
 module Cli exposing (run)
 
+import BackendTask exposing (BackendTask)
+import BackendTask.File
+import FatalError exposing (FatalError)
 import Pages.Script as Script exposing (Script)
 
 
@@ -7,3 +10,9 @@ run : Script
 run =
     Script.withoutCliOptions
         (Script.log "Hello from elm-pages Scripts!")
+
+
+readFile : String -> BackendTask FatalError String
+readFile filename =
+    BackendTask.File.rawFile filename
+        |> BackendTask.allowFatal
