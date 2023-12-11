9d0fe0620757ef872e1e20dc8c436a95170bf617
Jon Thompson <jon.thompson@vendr.com>
2023-12-11 11:59:17 -0500

    update newDay script to work with elm, init Day10

--- newDay.sh
+++ newDay.sh
@@ -2,6 +2,7 @@
 
 # Get the day number from the command line arguments
 day_number=$1
+file_type=$2
 
 # Check if day_number is not empty
 if [[ -z "$day_number" ]]; then
@@ -9,12 +10,19 @@ if [[ -z "$day_number" ]]; then
     exit 1
 fi
 
+# Check if file_type is not empty and is either 'roc' or 'elm'
+if [[ -z "$file_type" ]] || ([[ "$file_type" != "roc" ]] && [[ "$file_type" != "elm" ]]); then
+    echo "File type must be either 'roc' or 'elm'"
+    exit 1
+fi
+
 # Create two empty .txt files
 touch "Day${day_number}puzzle.txt"
 touch "Day${day_number}sample.txt"
 
-# Create a .roc file with the specified content
-cat << EOF > "Day${day_number}.roc"
+# Create a .roc or .elm file with the specified content
+if [[ "$file_type" == "roc" ]]; then
+    cat << EOF > "Day${day_number}.roc"
 app "hello"
     packages { pf: "https://github.com/roc-lang/basic-cli/releases/download/0.5.0/Cufzl36_SnJ4QbOoEmiJ5dIpUxBvdB3NEySvuH82Wio.tar.br" }
     imports [pf.Stdout
@@ -27,8 +35,19 @@ main =
     Stdout.line "I'm a Roc application!"
 EOF
 
-# Replace any roc filename on the 'roc test' line with 'Day${day_number}.roc' in ./.vscode/tasks.json
-sed -i "" "/roc test/s/Day[0-9]*\.roc/Day${day_number}.roc/" ./.vscode/tasks.json
-sed -i "" "/roc test/s/Day[0-9]*\.roc/Day${day_number}.roc/" ./testWatch.sh
+    # Replace any roc filename on the 'roc test' line with 'Day${day_number}.roc' in ./.vscode/tasks.json
+    sed -i "" "/roc test/s/Day[0-9]*\.roc/Day${day_number}.roc/" ./.vscode/tasks.json
+    sed -i "" "/roc test/s/Day[0-9]*\.roc/Day${day_number}.roc/" ./testWatch.sh
+
+elif [[ "$file_type" == "elm" ]]; then
+    cat << EOF > "./src/Day${day_number}.elm"
+module Main exposing (..)
+
+import Html exposing (text)
+
+main =
+    text "I'm an Elm application!"
+EOF
+fi
 
 echo "Files created successfully"
---
+++ src/Day10.elm
@@ -0,0 +1,6 @@
+module Main exposing (..)
+
+import Html exposing (text)
+
+main =
+    text "I'm an Elm application!"
