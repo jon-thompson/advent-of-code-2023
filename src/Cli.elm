fab211d9c47abf584923317e2b76a5904d6f6177
Jon Thompson <jon.thompson@vendr.com>
2023-12-11 11:48:12 -0500

    add runSolvers

--- src/Cli.elm
+++ src/Cli.elm
@@ -6,8 +6,20 @@ import FatalError exposing (FatalError)
 import Pages.Script as Script exposing (Script)
 
 
+type alias Solver =
+    String -> Int
+
+
 run : Script
 run =
+    runSolvers
+        { part1 = always 1
+        , part2 = always 2
+        }
+
+
+runSolvers : { part1 : Solver, part2 : Solver } -> Script
+runSolvers { part1, part2 } =
     Script.withoutCliOptions
         (BackendTask.combine
             [ readFile "Day07sample.txt"
@@ -16,8 +28,13 @@ run =
             |> BackendTask.andThen
                 (\files ->
                     case files of
-                        [ sample, input ] ->
-                            Script.log sample
+                        [ sample, puzzle ] ->
+                            Script.log <|
+                                "Part 1: "
+                                    ++ String.fromInt (part1 sample)
+                                    ++ "\n"
+                                    ++ "Part 2: "
+                                    ++ String.fromInt (part2 puzzle)
 
                         _ ->
                             Script.log "Wrong number of files loaded"
