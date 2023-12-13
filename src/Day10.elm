bf32f4afdde14e7b0d66e667bd6da14fd9b036d5
Jon Thompson <jon.thompson@vendr.com>
2023-12-13 11:53:08 -0500

    refactor to use Pipe type

--- src/Day10.elm
+++ src/Day10.elm
@@ -25,38 +25,90 @@ part2 input =
 
 
 type alias Grid =
-    List (List Char)
+    List (List (Maybe Pipe))
 
 
 type alias Position =
     ( Int, Int )
 
 
+{-| | is a vertical pipe connecting north and south.
+
+  - is a horizontal pipe connecting east and west.
+    L is a 90-degree bend connecting north and east.
+    J is a 90-degree bend connecting north and west.
+    7 is a 90-degree bend connecting south and west.
+    F is a 90-degree bend connecting south and east.
+    . is ground; there is no pipe in this tile.
+    S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+
+-}
+type Pipe
+    = Starting
+    | Vertical
+    | Horizontal
+    | NorthEast
+    | NorthWest
+    | SouthWest
+    | SouthEast
+
+
+charToMaybePipe : Char -> Maybe Pipe
+charToMaybePipe char =
+    case char of
+        '|' ->
+            Just Vertical
+
+        '-' ->
+            Just Horizontal
+
+        'L' ->
+            Just NorthEast
+
+        'J' ->
+            Just NorthWest
+
+        '7' ->
+            Just SouthWest
+
+        'F' ->
+            Just SouthEast
+
+        'S' ->
+            Just Starting
+
+        _ ->
+            Nothing
+
+
 parseGrid : String -> Grid
 parseGrid input =
     input
         |> String.lines
-        |> List.map String.toList
+        |> List.map (String.toList >> List.map charToMaybePipe)
 
 
-startingPosition : Grid -> Position
-startingPosition grid =
+startingPosition : String -> Position
+startingPosition input =
     let
-        positionsAndChars : List ( Position, Char )
+        grid =
+            parseGrid input
+
+        positionsAndChars : List ( Position, Maybe Pipe )
         positionsAndChars =
             grid
                 |> List.indexedMap
                     (\y row ->
                         row
                             |> List.indexedMap
-                                (\x char ->
-                                    ( ( x, y ), char )
+                                (\x maybePipe ->
+                                    ( ( x, y ), maybePipe )
                                 )
                     )
                 |> List.concat
     in
     positionsAndChars
-        |> List.filter (\( _, char ) -> char == 'S')
+        |> List.filter (\( _, maybePipe ) -> maybePipe == Just Starting)
         |> List.head
         |> Maybe.map (\( pos, _ ) -> pos)
         |> (\maybePos ->
@@ -78,15 +130,14 @@ suite =
 .FJ"""
                     |> parseGrid
                     |> Expect.equal
-                        [ [ '7', '-', 'F' ]
-                        , [ '.', 'F', 'J' ]
+                        [ [ Just SouthWest, Just Horizontal, Just SouthEast ]
+                        , [ Nothing, Just SouthEast, Just NorthWest ]
                         ]
         , test "startingPosition" <|
             \_ ->
-                [ [ '7', '-', 'F' ]
-                , [ 'S', 'F', 'J' ]
-                , [ '.', '7', 'R' ]
-                ]
+                """-L|F7
+7-7|S
+L|7|||"""
                     |> startingPosition
-                    |> Expect.equal ( 0, 1 )
+                    |> Expect.equal ( 4, 1 )
         ]
